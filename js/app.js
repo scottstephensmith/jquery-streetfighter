001c453377b0828be6c325471505d15bed3b3c47
Scott Smith <scottstephensmith@gmail.com>
2014-07-07 00:25:28 -0400

    animated project, without text

--- js/app.js
+++ js/app.js
@@ -17,17 +17,40 @@ $(document).ready(function() {
     	function() {
     		$(this).hide();
     		$(this).css('left', '-195px');
-  }
-);
+  		});
   })
   .mouseup(function() {
-  $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
-  });
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  })
+  // my custom keydown work starts here
+  $(document).keydown(function(event) {
+        if ( event.which == 88 ) {
+        console.log("X was pressed");	
+  		playLoop();
+  		$('.ryu-ready').hide();
+  		$('.ryu-cool').show();
+  	}
+  })
+  .keyup(function(key) {
+        if ( event.which == 88 ) {	
+        $('#loop-sound')[0].pause();
+      	$('#loop-sound')[0].load();
+  		$('.ryu-cool').hide();
+  		$('.ryu-still').show();
+  	}
+  })
 });
+
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
+}
+
+function playLoop () {
+  $('#loop-sound')[0].volume = 1;
+  $('#loop-sound')[0].load();
+  $('#loop-sound')[0].play();
 }
---
+++ js/app2.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken(); 
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate({'left': '300px'}, 
+      500, 
+      function() {
+        $(this).hide();
+        $(this).css('left', '-195px');
+      });
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+  // my custom keydown work starts here!!!!!!!!
+
+  var keysdown = {};
+  $(document).keydown(function(e) {
+      // So that the computer doesn't register multiple keypresses on keydown
+      if (e.keyCode == 88) {
+      if (keysdown[e.keyCode]) {
+        return;
+      }
+      keysdown[e.keyCode] = true;
+
+      // here's where we log and play things
+      console.log("X was pressed");	
+      playLoop();
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }
+    })
+  
+  .keyup(function(e) {
+    if (e.keyCode == 88) {
+        // Part of not registering multiple key presses  
+        delete keysdown[e.keyCode];
+
+        // stopping the music on keyup 
+        $('#loop-sound')[0].pause();
+        $('#loop-sound')[0].load();
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+      }
+    })
+});
+
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+function playLoop () {
+  $('#loop-sound')[0].volume = 1;
+  $('#loop-sound')[0].load();
+  $('#loop-sound')[0].play();
+}
